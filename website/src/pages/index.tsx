48e0e2e44acb9cfe8fdaad4eaea719abe413a33d
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:33:08 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the NixOS PatchELF executables">
+      description="A binary package with the NixOS PatchELF executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
