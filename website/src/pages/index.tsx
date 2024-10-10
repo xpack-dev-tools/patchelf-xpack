202ff024a8caebb565b6e89ec09c8eee619cf4bf
Liviu Ionescu <ilg@livius.net>
2024-10-10 21:37:27 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -41,9 +41,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from xPack Binary Development Tools`}
-      description="A binary package with the NixOS PatchELF executables">
+    <Layout 
+      title={`Hello from patchelf`}
+      description="A binary package with the patchelf executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
