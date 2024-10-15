9af95a23d9ac6f0e2cb47778ee7c84a4616c6731
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:34:06 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to patchelf`}
+      title="Welcome!"
       description="A binary package with the patchelf executables">
       <HomepageHeader />
       <main>
